327014dfd53cda12bf02d0276aff735df93e7d84
Oier Bravo Urtasun <oierbravo@gmail.com>
2018-03-11 04:03:34 +0100

    www: cosas basicas

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -1,25 +1,75 @@
 //document.addEventListener("deviceready", onDeviceReady, false);
 
-const serverURL = "192.168.1.5";
-
+const serverURL = "http://192.168.1.5";
+const deiaTimeout = 3000;
 function onDeviceReady() {
   console.log("DeviceReady triggered");
-  setInterval(function () {
+  $("#connected").hide();
+  $("#disconnected").hide();
+  $('.nori').hide();
+  $('.js-btn-ireki').click(sendIreki);
+  var deiakRef = firebase.database().ref('deiak').limitToLast(1);  
+  deiakRef.on('child_added', function(data) {
+     
+     var to = data.val().to;
+     if($('.nori').hasClass('firstTime')){
+       $('.nori').removeClass('firstTime');
+     } else {
+       $('.nori').text(to).data('ts',data.val().ts).fadeIn();
+       setTimeout(function(){ $('.nori').fadeOut() }, deiaTimeout);
+     }
+     
+   });
+   
+  /*setInterval(function () {
     $.ajax({
       url: serverURL + "/getStatus",
       type: 'GET',
       success: function(data){
-        $("#connected").show();
-        $("#disconnected").hide();
+        //$("#connected").removeClass('hidden');
+        $("#connected").fadeIn();
+        
+        //$('.spinner').addClass('hidden');
       },
       error: function(data) {
-        $("#connected").hide();
-        $("#disconnected").show();
+        //$("#disconnected").removeClass('hidden');
+        $("#disconnected").fadeIn();
+        
       }
     });
-  }, 1000);
+  }, 1000);*/
 }
-
+function sendIreki(ev){
+  var handler = function(event) {
+    showSnackbarButton.style.backgroundColor = '';
+  };
+  var snackbarContainer = document.querySelector('#demo-snackbar-example');
+  $.ajax({
+    url: serverURL + "/ireki",
+    type: 'GET',
+    success: function(data){
+      var data = {
+        message: 'Abrir OK.',
+        timeout: 2000,
+        actionHandler: handler,
+        actionText: 'Undo'
+      };
+      snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    },
+    error: function(data) {
+      var data = {
+        message: 'Error al abrir.',
+        timeout: 2000,
+        actionHandler: handler,
+        actionText: 'Undo'
+      };
+      
+      snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    }
+  });
+  
+}
 $(function () {
   onDeviceReady();
+  
 })
