b966f1c5cb503babc166ddcfa8ac9de7a14f2d28
Oier Bravo Urtasun <oierbravo@gmail.com>
2018-03-31 19:07:10 +0200

    doorStatus, keypad debounce and delays...

--- firmware/photon/txirrina.ino
+++ firmware/photon/txirrina.ino
@@ -1,3 +1,7 @@
+// This #include statement was automatically added by the Particle IDE.
+//#include <Keypad_Particle.h>
+
+
 /*********************************************************
 Serial Graphic LCD Library Demo
 Joel Bartlett
@@ -18,10 +22,17 @@ that this code will work on any other LCDs or backpacks.
 
 ***********************************************************/
 
-#define RELAY_PIN 2
-#define RELAY_LED_PIN 3
+#define RELAY_PIN 6
+#define RELAY_LED_PIN 7
 #define ENABLE_RELAY_LED
 #define RELAY_DELAY 2000
+#define DEBOUNCE_DELAY 2
+
+#define DOOR_ENDSTOP_PIN 5
+int doorValue;
+int lastDoorValue;
+
+#define KEYPRESS_DELAY 500
 
 #include "SparkFun_Serial_Graphic_LCD.h"//inculde the Serial Graphic LCD library
 
@@ -40,21 +51,22 @@ that this code will work on any other LCDs or backpacks.
 //Create an instance of the LCD class named LCD. We will use this instance to call all the
 //subsequent LCD functions
 LCD LCD;
-const unsigned long period = 50;
+const unsigned long period = 200;
 unsigned long prevMillis = 0;
- 
-byte iRow = 0, iCol = 0;
+
+
+int iRow = 0, iCol = 0;
 const byte countRows = 4;
 const byte countColumns = 4;
- 
-const byte rowsPins[countRows] = { A1, A2, A3, A4 };
-const byte columnsPins[countColumns] = { 7, 6, 5, 4 };
- 
+
+const byte rowsPins[countRows] = { A0, A1, A2, A3 };
+const byte columnsPins[countColumns] = { A4, A5, A6, A7 };
+
 String keys[countRows][countColumns] = {
    { "1","2","3", "A" },
    { "4","5","6", "B" },
    { "7","8","9", "C" },
-   { "#","0","*", "D" }
+   { "*","0","#", "D" }
 };
 
 String keyBuffer;
@@ -62,24 +74,34 @@ String keyBuffer;
 bool readKeypad()
 {
    // Barrer todas las filas
-   for (byte r = 0; r < countRows; r++)
+   for (int r = 0; r < countRows; r++)
    {
     // pinMode(rowsPins[r], OUTPUT);
       digitalWrite(rowsPins[r], LOW);      // Poner en LOW fila
- 
+
       // Comprobar la fila
-      for (byte c = 0; c < countColumns; c++)
+      for (int c = 0; c < countColumns; c++)
       {
+
          if (digitalRead(columnsPins[c]) == LOW)   // Pulsacion detectada
          {
+            #ifdef DEBOUNCE_DELAY
+            delay(DEBOUNCE_DELAY);
+            byte delayRead = digitalRead(columnsPins[c]);
+            if(delayRead == LOW){
+             #endif
             iRow = r;
             iCol = c;
             return true;
+            #ifdef DEBOUNCE_DELAY
+            }
+            #endif
          }
       }
-      
+
     //  pinMode(rowsPins[r], HIGH);         // Ponen en HIGH fila
-      digitalWrite(rowsPins[r], INPUT);   // Poner en alta impedancia
+      digitalWrite(rowsPins[r], HIGH);   // Poner en alta impedancia
+      delay(10);
    }
    return false;
 }
@@ -91,6 +113,10 @@ pinMode(RELAY_PIN,OUTPUT);
     pinMode(RELAY_LED_PIN,OUTPUT);
 #endif
 Particle.function("ireki",handleIreki);
+
+pinMode(DOOR_ENDSTOP_PIN,INPUT_PULLUP);
+Particle.variable("doorOpen", &doorValue, INT);
+
 //we're just going to run through a bunch of demos to show the functionality of the LCD.
 
 delay(1200);///wait for the one second spalsh screen before anything is sent to the LCD.
@@ -99,7 +125,7 @@ LCD.setHome();//set the cursor back to 0,0.
 LCD.clearScreen();//clear anything that may have been previously printed ot the screen.
 delay(10);
 
-LCD.print("Txirina! //Ongi etorri gure zerbitzura//");
+LCD.println("Txirina! //Ongi etorri gure zerbitzura//");
 delay(1500);
 
 //This function should only be used if you accidentally changed the baud rate or if you forgot to what rate it was changed.
@@ -107,7 +133,7 @@ delay(1500);
 
 
 Serial.begin(9600);
- 
+
    for (byte c = 0; c < countColumns; c++)
    {
       pinMode(columnsPins[c], INPUT_PULLUP);
@@ -125,22 +151,49 @@ void loop()
     if (millis() - prevMillis > period)   // Espera no bloqueante
    {
       prevMillis = millis();
+
+      doorValue = !digitalRead(DOOR_ENDSTOP_PIN);
+      if(lastDoorValue != doorValue){
+          if(doorValue == HIGH){
+              Particle.publish("doorStatus","true");
+          } else {
+              Particle.publish("doorStatus","false");
+          }
+
+      }
+      lastDoorValue = doorValue;
       if (readKeypad())   // Detección de tecla pulsada
       {
           //Serial.println(iRow);
-          
+
           String currentKey=keys[iRow][iCol];
+          //Particle.publish("key",currentKey);
+          //LCD.print(String(currentKey) + "");
           if(currentKey == "#"){
-            Particle.publish("key",keyBuffer);
+            Particle.publish("deia",keyBuffer);
             keyBuffer = "";
+            //LCD.print("Deia!");
+            LCD.clearScreen();
+            LCD.print("");
           } else if (currentKey == "*") {
             keyBuffer = "";
+            LCD.clearScreen();
           } else {
-            keyBuffer += currentKey;
+            keyBuffer = keyBuffer + "" + currentKey;
+            char output[keyBuffer.length()];
+            currentKey.toCharArray(output, keyBuffer.length());
+            LCD.clearScreen();
+            //LCD.setHome();
+            Serial1.print(keyBuffer);
+            delay(KEYPRESS_DELAY);
           }
-         
-         LCD.print(*keyBuffer);   // Mostrar tecla
-         delay(100);
+          if(keyBuffer == ""){
+              //delay(1000);
+              //LCD.clearScreen();
+          }
+
+//LCD.print(*keyBuffer);   // Mostrar tecla
+         //delay(100);
       }
    }
 }
@@ -155,5 +208,5 @@ int handleIreki(String command) {
     #ifdef ENABLE_RELAY_LED
         digitalWrite(RELAY_LED_PIN,LOW);
     #endif
+    return 1;
 }
-
